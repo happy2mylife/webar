3ced4c0744e57d204a943ecf663db8255b3f720c
h2m_kinoko <happy2mylife@gmail.com>
2019-02-15 09:01:03 +0900

    click

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function onLoad() {
   marker.addEventListener('markerFound', () => {});
 
   const carObj = document.getElementById('car-obj');
-  carObj.addEventListener('touchstart', () => {
+  carObj.addEventListener('click', () => {
     alert('touched.');
   });
 }
