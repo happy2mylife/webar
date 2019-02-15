5aeb680d994a51a85fdc232f089a9fb5224d9d1d
h2m_kinoko <happy2mylife@gmail.com>
2019-02-15 12:45:20 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function onLoad() {
   $('.main-container').on('click', function() {
-    $(this).css('visibility', 'hidden');
+//    $(this).css('visibility', 'hidden');
+    $(this).css('display', 'none');
     $(this).css('z-index', 0);
     $('a-scene').css('z-index', 1);
   });
