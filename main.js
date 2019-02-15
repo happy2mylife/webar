57450548b278172308929be58073ef43babebc9b
h2m_kinoko <happy2mylife@gmail.com>
2019-02-15 12:14:27 +0900

    hidden

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
 function onLoad() {
   $('.main-container').on('click', function() {
-    $('.main-container')
-      .fadeOut('slow')
-      .queue(() => {
-        $('.main-container').css('display', 'hidden');
-        $('a-scene').css('display', 'visible');
-        const marker = document.getElementById('anchor');
-        marker.addEventListener('markerFound', () => {
-          const rect = carObj.getBoundingClientRect();
-          alert(`${rect.top} : ${rect.left} : ${rect.bottom} ; ${rect.right}`);
-        });
-      });
+    $('.main-container').css('display', 'hidden');
   });
 }
