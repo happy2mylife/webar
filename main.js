a0e1dda26b3399247f2ad3eba6c6cbaa4d149e29
h2m_kinoko <happy2mylife@gmail.com>
2019-02-15 11:38:27 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
 function onLoad() {
-  const carObj = document.querySelector('#id-car-obj');
-  carObj.addEventListener('click', () => {
-    alert('click.');
-  });
-  carObj.addEventListener('touchend', () => {
-    alert('touched.');
-  });
-
   const marker = document.getElementById('anchor');
   marker.addEventListener('click', () => {
     alert('click.');
