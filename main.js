43921e6700249a8921c87763fe2107a38bc3c2ae
h2m_kinoko <happy2mylife@gmail.com>
2019-02-15 11:05:22 +0900

    marker.

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 function onLoad() {
   const carObj = document.querySelector('#id-car-obj');
   carObj.addEventListener('click', () => {
-    alert('touched.');
+    alert('click.');
   });
   carObj.addEventListener('touchend', () => {
     alert('touched.');
   });
 
   const marker = document.getElementById('anchor');
+  marker.addEventListener('click', () => {
+    alert('click.');
+  });
+  marker.addEventListener('touchend', () => {
+    alert('touched.');
+  });
   marker.addEventListener('markerFound', () => {
     const rect = carObj.getBoundingClientRect();
     alert(`${rect.top} : ${rect.left} : ${rect.bottom} ; ${rect.right}`);
