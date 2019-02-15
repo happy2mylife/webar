a8d6de9e08057852e72c55389c24e6a2082a29b7
h2m_kinoko <happy2mylife@gmail.com>
2019-02-15 10:56:26 +0900

    rect

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 function onLoad() {
-  const marker = document.getElementById('anchor');
-  marker.addEventListener('markerFound', () => {});
-
   const carObj = document.querySelector('#id-car-obj');
   carObj.addEventListener('click', () => {
     alert('touched.');
   });
   carObj.addEventListener('touchend', () => {
     alert('touched.');
+  });
+
+  const marker = document.getElementById('anchor');
+  marker.addEventListener('markerFound', () => {
+    const rect = carObj.getBoundingClientRect();
+    alert(`${rect.top} : ${rect.left} : ${rect.bottom} ; ${rect.right}`);
   });
 }
