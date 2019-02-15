3127c023d170c4f3730339a40ef8b4092ba7e0c0
h2m_kinoko <happy2mylife@gmail.com>
2019-02-15 11:35:43 +0900

    fade out

--- main.js
+++ main.js
@@ -18,4 +18,8 @@ function onLoad() {
     const rect = carObj.getBoundingClientRect();
     alert(`${rect.top} : ${rect.left} : ${rect.bottom} ; ${rect.right}`);
   });
+
+  $('.main-container').on('click', function() {
+    $('.main-container').fadeOut('slow');
+  });
 }
