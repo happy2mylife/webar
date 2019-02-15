2cc4904e2b9dfdeec09f19d24987201a4d399dd2
h2m_kinoko <happy2mylife@gmail.com>
2019-02-15 11:57:26 +0900

    Update main.js

--- main.js
+++ main.js
@@ -5,14 +5,7 @@ function onLoad() {
       .queue(() => {
         $('.main-container').attr('display', 'hidden');
         $('a-scene').atrr('display', 'visible');
-
         const marker = document.getElementById('anchor');
-        marker.addEventListener('click', () => {
-          alert('click.');
-        });
-        marker.addEventListener('touchend', () => {
-          alert('touched.');
-        });
         marker.addEventListener('markerFound', () => {
           const rect = carObj.getBoundingClientRect();
           alert(`${rect.top} : ${rect.left} : ${rect.bottom} ; ${rect.right}`);
