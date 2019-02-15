c78ced89683ba869fceedb0ae413fd0fb6929325
h2m_kinoko <happy2mylife@gmail.com>
2019-02-15 14:46:11 +0900

    marker lost

--- main.js
+++ main.js
@@ -9,4 +9,8 @@ function onLoad() {
       $('.controller').css('z-index', 110);
     });
   });
+  $('#marker')[0].addEventListener('markerLost', () => {
+    $('.controller').css('z-index', -50);
+    $('.controller').css('display', 'none');
+  });
 }
