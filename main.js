38086f519c90202f33b52f1a776b4829e94aee6c
h2m_kinoko <happy2mylife@gmail.com>
2019-02-15 11:49:45 +0900

    add

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 function onLoad() {
-  const marker = document.getElementById('anchor');
-  marker.addEventListener('click', () => {
-    alert('click.');
-  });
-  marker.addEventListener('touchend', () => {
-    alert('touched.');
-  });
-  marker.addEventListener('markerFound', () => {
-    const rect = carObj.getBoundingClientRect();
-    alert(`${rect.top} : ${rect.left} : ${rect.bottom} ; ${rect.right}`);
-  });
-
   $('.main-container').on('click', function() {
-    $('.main-container').fadeOut('slow');
+    $('.main-container')
+      .fadeOut('slow')
+      .queue(() => {
+        $('.main-container').attr('display', 'hidden');
+        $('a-scene').atrr('display', 'visible');
+
+        const marker = document.getElementById('anchor');
+        marker.addEventListener('click', () => {
+          alert('click.');
+        });
+        marker.addEventListener('touchend', () => {
+          alert('touched.');
+        });
+        marker.addEventListener('markerFound', () => {
+          const rect = carObj.getBoundingClientRect();
+          alert(`${rect.top} : ${rect.left} : ${rect.bottom} ; ${rect.right}`);
+        });
+      });
   });
 }
