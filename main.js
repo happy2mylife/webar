3b1fd4507a00d04f0dc1a01d0060c0392c70864b
h2m_kinoko <happy2mylife@gmail.com>
2019-02-15 13:01:24 +0900

    a

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function onLoad() {
   $('.main-container').on('click', function() {
-//    $(this).css('visibility', 'hidden');
+    //    $(this).css('visibility', 'hidden');
     $(this).css('display', 'none');
     $(this).css('z-index', 0);
-    $('a-scene').css('z-index', 1);
+    $('a-scene').css('z-index', 999);
   });
 }
