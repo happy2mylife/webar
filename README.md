b9fa7d0364520b91fe240950fb49a650fc39da20
testsobalmaster <testsobalmaster@gmail.com>
2020-03-05 09:52:46 +0900

    use three js.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# webar
+# use three js
 
 - published on netlify
+
+
+
+# 
+- three.js で読み込むオブジェクトごとにローダーが必要
+  - https://github.com/mrdoob/three.js/blob/dev/examples/js/loaders/OBJLoader.js
--- main.js
+++ main.js
@@ -1,27 +1,48 @@
-let isStarted = false;
+let scene;
+let camera;
+let renderer;
+let cube;
+let objLoader;
+
 function onLoad() {
-  $('.start').on('click', function() {
-    $('.main-container').remove();
-    $('a-scene').css('z-index', 100);
-    isStarted = true;
+  objLoader = new THREE.OBJLoader();
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.set(0, 0, +1000);
+
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+  var geometry = new THREE.CubeGeometry(1, 1, 1);
+  var material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+  var light = new THREE.DirectionalLight(0xffffff);
+  light.intensity = 2;
+  light.position.set(1, 1, 1);
+
+  cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+  scene.add(light);
+  camera.position.z = 5;
+  objLoader.load("./obj/text.obj", (obj) => {
+    scene.add(obj);
   });
 
-  $('#marker')[0].addEventListener('markerFound', () => {
-    // スタートボタン押す前も裏でカメラが動いていて検知しているため
-    // スタート押されるまでは何もしない
-    if (!isStarted) {
-      return;
-    }
-    $('.controller').fadeIn('slow', () => {
-      $('.controller').css('z-index', 110);
-    });
-  });
-  $('#marker')[0].addEventListener('markerLost', () => {
-    $('.controller').css('z-index', -50);
-    $('.controller').css('display', 'none');
-  });
+  render();
+  tick();
 
-  $('#play').on('click', () => {
-    $('#video').play();
-  });
+}
+
+function render() {
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+}
+
+
+
+function tick() {
+  requestAnimationFrame(tick);
+  cube.rotation.y += 0.01;
+  cube.rotation.x += 0.01;
+  renderer.render(scene, camera);
 }
